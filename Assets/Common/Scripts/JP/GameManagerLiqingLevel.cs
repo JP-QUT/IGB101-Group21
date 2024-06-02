399dced6db9b6756895face3941d73f9730aa563
JP-QUT <n11336048@qut.edu.au>
2024-06-02 15:00:24 +1000

    Update
    Updated my level. Currently trying to figure out how to transition scenes with different systems.

---
+++ Assets/Common/Scripts/JP/GameManagerLiqingLevel.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManagerLiqingLevel : MonoBehaviour
+{
+    public GameObject player;
+    
+    // Liqing's Level has a completely different system
+
+
+    // Manually set to true to allow level switching
+    public bool levelComplete = true;
+
+    void Update()
+    {
+
+    }
+}
+
---
+++ Assets/Common/Scripts/JP/LevelDesign/LevelSwitchLiqing.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelSwitchLiqing : MonoBehaviour
+{
+    GameManager gameManager;
+    public string nextLevel;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        gameManager = GameObject.FindGameObjectWithTag("GameManager").GetComponent<GameManager>();
+    }
+
+    private void OnTriggerEnter(Collider otherObject)
+    {
+        if(otherObject.transform.CompareTag("Player"))
+        {
+        SceneManager.LoadScene(nextLevel);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
